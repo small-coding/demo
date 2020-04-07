b842df0997c83e7f9533ced0b09e35710bf7f538
small-coding <javads945@163.com>
2020-04-07 11:20:41 +0800

    .\HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+	    	System.out.println("HelloWorld!");
+		System.out.println("1");
+		System.out.println("2!");
+		System.out.println("3");
+		System.out.println("java");
+	}
+}
